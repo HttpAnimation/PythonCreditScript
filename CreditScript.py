9cd9ce86ac279cb89ad891d44f670bb9d1856dd2
HttpAnimation <97435656+HttpAnimation@users.noreply.github.com>
2022-01-27 01:07:43 -0700

    Create CreditScript.py

---
+++ CreditScript.py
@@ -0,0 +1,7 @@
+import time
+print("Type App/Game Name here")
+time.sleep(2.5)
+print("Made by: Type Your name here")
+
+"Script made by: https://www.youtube.com/channel/UCGsVJH8qR83TSUjB7p1Wg0A"
+"type your stuff here"
